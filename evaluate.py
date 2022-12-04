5dc224b90c99e27d98f5a1f3e6b8d04931ec9f32
Harsh Trivedi <harshjtrivedi94@gmail.com>
2022-12-04 13:10:59 -0500

    make evaluate.py more extensive.

--- evaluate.py
+++ evaluate.py
@@ -1,4 +1,5 @@
 from typing import List, Dict
+from collections import Counter, defaultdict
 import argparse
 import json
 import os
@@ -17,14 +18,25 @@ from predictions_to_official_format import (
 )
 
 
-os.environ["TOKENIZERS_PARALLELISM"] = "false"
-
-
 def compute_answer_scores(prediction_instances: List[Dict]) -> Dict:
 
-    answer_text_metrics = ListSquadEmAndF1(keep_whitespace=True)
+    answer_text_metrics = defaultdict(lambda: ListSquadEmAndF1(keep_whitespace=True))
+
+    category_overall = ("overall", "overall")
+
+    categories_overall = set([category_overall])
+    categories_num_steps = set()
+    categories_answer_type = set()
+    categories_task_name = set()
+    categories_program_type = set()
+
+    category_counter = Counter()
 
     for instance in prediction_instances:
+
+        answer_type = instance.get("answer_type", None)
+        if answer_type is None:
+            answer_type = instance.get("reasoning_type", None)
 
         all_answer_texts = None
         if "all_answer_texts" in instance:
@@ -39,30 +51,89 @@ def compute_answer_scores(prediction_instances: List[Dict]) -> Dict:
                 for answer_object in instance["answers_objects"]
             ]
 
-        if "answers" in instance:  # It's a list of validated answers.
+            answers_object = instance["answers_objects"][0]
+            if answer_type is not None:
+                pass
+            elif "number" in answers_object and answers_object["number"]:
+                answer_type = "number"
+            elif "spans" in answers_object and answers_object["spans"]:
+                answer_type = "spans"
+            elif "date" in answers_object:
+                answer_type = "date"
+            else:
+                raise Exception("Answer type couldn't be determined.")
+
+        if "answers" in instance: # It's a list of validated answers.
             for answer in instance["answers"]:
                 assert isinstance(answer, str)
             all_answer_texts = [(answer,) for answer in instance["answers"]]
 
-        if (
-            "answer_list" in instance
-        ):  # It's a single "answer" (which is a list of entities/etc)
-            assert isinstance(instance["answer_list"], tuple) or isinstance(
-                instance["answer_list"], list
-            )
+        if "answer_list" in instance: # It's a single "answer" (which is a list of entities/etc)
+            assert isinstance(instance["answer_list"], tuple) or isinstance(instance["answer_list"], list)
             all_answer_texts = [tuple(instance["answer_list"])]
 
         assert all_answer_texts is not None
 
         predicted_answers = instance["predicted_answers"]
-        answer_text_metrics(predicted_answers, all_answer_texts)
 
-    metric_values = answer_text_metrics.get_metric(reset=False)
+        if "program_modules" not in instance:
+            program_type = "unknown"
+            num_steps = "unknown"
+            task_name = "real_qa"
+        else:
+            program_modules = instance["program_modules"]
+            program_type = "__".join(program_modules)
+            num_steps = len(program_modules)
+            task_name = "teabreac_primitive_qa" if num_steps == 1 else "teabreac_multistep_qa"
 
-    result = {
-        "ans_em": round(metric_values["ans_em"], 3),
-        "ans_f1": round(metric_values["ans_f1"], 3),
+        category_num_steps = ("num_steps", num_steps)
+        category_answer_type = ("answer_type", answer_type)
+        category_task_name = ("task_name", task_name)
+        category_program_type = ("program_type", program_type)
+
+        categories_num_steps.add(category_num_steps)
+        categories_answer_type.add(category_answer_type)
+        categories_task_name.add(category_task_name)
+        categories_program_type.add(category_program_type)
+
+        key_tuples = [
+            category_overall, category_num_steps,
+            category_answer_type, category_task_name,
+            category_program_type
+        ]
+
+        for key_tuple in key_tuples:
+            if predicted_answers is not None:
+                answer_text_metrics[key_tuple](predicted_answers, all_answer_texts)
+            category_counter[key_tuple] += 1
+
+    all_key_tuples = (
+        sorted(categories_overall, key=lambda e: e[1]) +
+        sorted(categories_num_steps, key=lambda e: e[1]) +
+        sorted(categories_answer_type, key=lambda e: e[1]) +
+        sorted(categories_task_name, key=lambda e: e[1]) +
+        sorted(categories_program_type, key=lambda e: e[1])
+    )
+
+    answer_text_em_metric_values = {key_tuple: round(metric.get_metric(reset=False)["ans_em"], 3)
+                                    for key_tuple, metric in answer_text_metrics.items()}
+    answer_text_f1_metric_values = {key_tuple: round(metric.get_metric(reset=False)["ans_f1"], 3)
+                                    for key_tuple, metric in answer_text_metrics.items()}
+    data = {
+        "category": [e[0] for e in all_key_tuples],
+        "subcategory": [e[1] for e in all_key_tuples],
+        "answer_em": [answer_text_em_metric_values.get(key_tuple, 0.0) for key_tuple in all_key_tuples],
+        "answer_f1": [answer_text_f1_metric_values.get(key_tuple, 0.0) for key_tuple in all_key_tuples],
+        "counts": [category_counter[key_tuple] for key_tuple in all_key_tuples]
     }
+    # You can load this data in pandas like
+    # print(pd.from_dict(data).to_string())
+    # and get a detailed summary of results in various categories, subcategories.
+
+    ans_em = answer_text_em_metric_values[category_overall]
+    ans_f1 = answer_text_f1_metric_values[category_overall]
+
+    result = {"ans_em": ans_em, "ans_f1": ans_f1, "data": data}
     return result
 
 
@@ -126,10 +197,12 @@ def compute_answer_scores_with_official_scripts(
         type_to_em = {}
         type_to_f1 = {}
         for num in range(1, 8 + 1):
-            for _, subdata in metrics_data.iterrows():
-                if subdata["subcategory"].lower() == f"type_{num}":
-                    type_to_em[f"type_{num}"] = subdata["answer_em"]
-                    type_to_f1[f"type_{num}"] = subdata["answer_f1"]
+            for subcategory, ans_em, ans_f1 in zip(
+                metrics_data["subcategory"], metrics_data["answer_em"], metrics_data["answer_f1"]
+            ):
+                if subcategory.lower() == f"type_{num}":
+                    type_to_em[f"type_{num}"] = ans_em
+                    type_to_f1[f"type_{num}"] = ans_f1
             assert f"type_{num}" in type_to_em
 
         metrics = {}
